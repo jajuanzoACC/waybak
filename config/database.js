6fcaf359440777ca48eff5442de0430248509eec
Joshua <jajuanzo@affinitycorp.net>
2022-07-24 05:48:14 +0800

    edit database config

--- config/database.js
+++ config/database.js
@@ -1,11 +1,21 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "sqlite",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
     },
     useNullAsDefault: true,
+    ssl: {
+      rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+    },
+    options: {
+      ssl: env.bool("DATABASE_SSL", false),
+    },
   },
 });
